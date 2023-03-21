a7945e0333eb77d3d7b12c9903dc3fc449bf020c
puffick24 <kowaleff.evgeniy2014@yandex.ru>
2023-03-21 11:37:05 +0300

    added axios request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import axios from 'axios';
+
 function App() {
+
+  axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=fb59bcb5')
+  .then(response => console.log(response))
+  .catch(error => console.log(error.message))
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      
     </div>
   );
 }
