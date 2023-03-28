543564678ce8abd54e9a859a7c0f3b1e55d2fae3
puffick24 <kowaleff.evgeniy2014@yandex.ru>
2023-03-28 20:04:10 +0300

    made rating

--- src/Components/FilmPage/Rating.js
+++ src/Components/FilmPage/Rating.js
@@ -1,14 +1,23 @@
 import React from "react";
 import activeStarImg from "../../images/full.svg";
 import inactiveStarImg from "../../images/empty.svg";
+import halfStarImg from "../../images/halfStar.svg";
 
 const StarRating = ({ rating }) => {
+  const fullStars = Math.floor(rating);
+  const decimal = rating - fullStars;
   const stars = []
 
   for (let i = 0; i < 10; i++) {
-    const isActive = i < rating;
-    const starImg = isActive ? activeStarImg : inactiveStarImg;
+    let starImg = inactiveStarImg;
 
+    if (i < fullStars) {
+      starImg = activeStarImg;
+    } else if (i === fullStars && decimal >= 0.8) {
+      starImg = activeStarImg;
+    } else if (i === fullStars && decimal >= 0.3) {
+      starImg = halfStarImg;
+    }
     stars.push(
       <img
         key={i}
